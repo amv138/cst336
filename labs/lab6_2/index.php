4d0ebdb419b874587da4aab9b6e7b5821d0dc707
Angel Vasquez <anvasquez@csumb.edu>
2019-03-24 05:55:03 +0000

    adding final lab 6 changes

--- labs/lab6_2/index.php
+++ labs/lab6_2/index.php
@@ -143,7 +143,7 @@
                   <div class="modal-dialog" role="document">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h5 class="modal-title">Modal title</h5>
+                        <h5 class="modal-title">Purchase History</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                         </button>
