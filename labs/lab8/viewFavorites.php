a52a7d07fc4f83ddee62454a76dff63d3f82653b
Angel Vasquez <anvasquez@csumb.edu>
2019-04-27 23:10:37 +0000

    adding bootstrap changes

--- labs/lab8/viewFavorites.php
+++ labs/lab8/viewFavorites.php
@@ -6,6 +6,7 @@
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
 <script>
     $.ajax({
         type: "GET",
